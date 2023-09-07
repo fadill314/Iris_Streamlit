2af53ac04d29f64adc32938640ed2b8a01e590f2
fadill314 <58606938+fadill314@users.noreply.github.com>
2023-09-07 22:58:23 +0100

    Update irisap.py

--- irisap.py
+++ irisap.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-st.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
+#st.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
 st.image("http://www.ehtp.ac.ma/images/lo.png")
 st.write("""
 # MSDE4 : ML Course
